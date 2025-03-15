81049941efbb6cd9ad936ed4c663e4970f64dc19
Dominguez <teo.dominguez.782@my.csun.edu>
2025-03-14 18:09:04 -0700

    3/14/25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { WeatherComponent } from './weather/weather.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CountryPopulationComponent } from './country-population/country-population.component';
 
 export const routes: Routes = [
     { path:"weather", component:WeatherComponent },
     { path: "cities", component:CitiesComponent },
     { path: "countries", component:CountriesComponent },
     { path:"navbar", component:NavBarComponent },
+    { path: "countryPopulation/:id", component:CountryPopulationComponent },
     { path:"", component:WeatherComponent, pathMatch: "full" }
+    
 ];
